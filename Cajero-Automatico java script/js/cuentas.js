fbf00b9195363c0d3caadb38a8163c9159090481
Carlitus29 <106565899+Carlitus29@users.noreply.github.com>
2022-07-28 19:13:42 -0400

    Add files via upload

---
+++ Cajero-Automatico java script/js/cuentas.js
@@ -0,0 +1,17 @@
+const cuentas = [{
+        nombre: 'carlos',
+        password: 'carlos1',
+        saldo: 900
+    },
+    {
+        nombre: 'carla',
+        password: 'carla2',
+        saldo: 500
+    },
+
+    {
+        nombre: 'job',
+        password: 'job3',
+        saldo: 30
+    }
+]
